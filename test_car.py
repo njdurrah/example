b6c42c5f92d933433316ec33251cbb9268e31825
ND <njdurrah@berkeley.edu>
2020-04-19 11:23:35 -0700

    added test_accelerate

--- test_car.py
+++ test_car.py
@@ -17,3 +17,14 @@ class TestInit(TestCar):
 
     def test_initial_time(self):
         self.assertEqual(self.car.time, 0)
+
+
+class TestAccelerate(TestCar):
+    def test_accelerate_from_zero(self):
+        self.car.accelerate()
+        self.assertEqual(self.car.speed, 5)
+
+    def test_multiple_accelerates(self):
+        for _ in range(3):
+            self.car.accelerate()
+        self.assertEqual(self.car.speed, 15)
